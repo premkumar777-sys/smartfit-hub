8db5774e44ab892d399bbf78f716a083a38ba2a1
Prem <eslavathpremkumar17@gmail.com>
2025-12-17 23:47:51 +0530

    ene

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ import HomeWorkouts from "./pages/HomeWorkouts";
 import AITrainer from "./pages/AITrainer";
 import Gamification from "./pages/Gamification";
 import NotFound from "./pages/NotFound";
-import { EnergyCursor } from "@/components/EnergyCursor";
+import { AICursor } from "@/components/AICursor";
 
 const queryClient = new QueryClient();
 
@@ -26,7 +26,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
-        <EnergyCursor />
+        <AICursor />
         <Header />
         <Routes>
           <Route path="/" element={<Home />} />
