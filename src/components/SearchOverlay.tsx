b0ec27b2f58879e2eb060092969b30cdb63d78bf
Prem <eslavathpremkumar17@gmail.com>
2025-12-25 09:57:02 +0530

    pay

--- src/components/SearchOverlay.tsx
+++ src/components/SearchOverlay.tsx
@@ -112,3 +112,5 @@ export function SearchOverlay({ isOpen, onClose }: SearchOverlayProps) {
     </>
   );
 }
+
+
--- src/components/dropdowns/index.ts
+++ src/components/dropdowns/index.ts
@@ -2,3 +2,5 @@ export { default as FeaturesDropdown } from './FeaturesDropdown';
 export { default as GymsDropdown } from './GymsDropdown';
 export { default as MarketplaceDropdown } from './MarketplaceDropdown';
 export { default as BusinessDropdown } from './BusinessDropdown';
+
+
