69242f70960b34298cf5e9086a10a6aff4fe7cec
Prem <eslavathpremkumar17@gmail.com>
2026-01-22 20:11:40 +0530

    Re-enable payments - subscription system working

--- src/config.ts
+++ src/config.ts
@@ -2,8 +2,7 @@
 // Global Configuration
 
 // toggle this to false to disable all paywalls (Beta Mode)
-// TEMP: Set to false while fixing auth - user has paid ₹99
-export const ENABLE_PAYMENTS = false;
+export const ENABLE_PAYMENTS = true;
 
 // Feature Flags
 export const ENABLE_B2B_FEATURES = true;
