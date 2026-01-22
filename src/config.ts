f0d7f1420b1bbd56b99e3f3989032f6096ea1861
Prem <eslavathpremkumar17@gmail.com>
2026-01-22 18:52:06 +0530

    Temporarily disable payments - user paid but auth issues

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,8 @@
 // Global Configuration
 
 // toggle this to false to disable all paywalls (Beta Mode)
-export const ENABLE_PAYMENTS = true;
+// TEMP: Set to false while fixing auth - user has paid ₹99
+export const ENABLE_PAYMENTS = false;
 
 // Feature Flags
 export const ENABLE_B2B_FEATURES = true;
