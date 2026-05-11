be9cc019974449637f12bf5881f3f6d926109d2b
Prem <eslavathpremkumar17@gmail.com>
2026-05-11 23:06:28 +0530

    login

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.rv5uj0gde2"
+    "revision": "0.ultupuheuoo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -180,6 +180,7 @@ export default function Auth() {
           redirectTo: redirectUrl,
           queryParams: {
             access_type: 'offline',
+            prompt: 'consent',
           },
         },
       });
