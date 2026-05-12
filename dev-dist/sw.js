658154e65b7c0bbd904e9aa287fb828e5f6e2d1d
Prem <eslavathpremkumar17@gmail.com>
2026-05-12 13:16:40 +0530

    featurees

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.52jkfp67hdc"
+    "revision": "0.otn747ccl6g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
