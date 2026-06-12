3835c0c294aabab4557e3be77cfa0a9895568cbf
Prem <eslavathpremkumar17@gmail.com>
2026-06-12 15:27:33 +0530

    andriod

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.kqslrk293vg"
+    "revision": "0.pjrvo426rq4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
