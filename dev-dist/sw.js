b7a7883dac33a3f975cf9c90d75f308ede40de5d
Prem <eslavathpremkumar17@gmail.com>
2026-06-17 11:18:00 +0530

    coach

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.ecphgevpbmc"
+    "revision": "0.sqlns6hc6fg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
